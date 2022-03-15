d567b6cfe6bdd8b44a778e7d2d18df4632ad20ea
Daniel J Crowley <61795248+TheMusicMan28@users.noreply.github.com>
2022-03-15 13:40:39 -0500

    added animation tree and began working on player movement

--- Player.gd
+++ Player.gd
@@ -1,37 +1,25 @@
 extends KinematicBody2D
 
 
-export var speed = 200
+const ACCELERATION = 500
+const MAX_SPEED = 200
+
 
 var velocity = Vector2.ZERO
+var aniPlayer
+
+onready var aniTree = $AnimationTree
+onready var aniState = aniTree.get('parameters/playback')
 
 func _ready():
-	pass # Replace with function body.
-
-# Movement
-func get_input():
-	# Detect up/down/left/right keystate and only move when pressed.
-	velocity = Vector2()
-	if Input.is_action_pressed('ui_right'):
-		velocity.x += 1
-	if Input.is_action_pressed('ui_left'):
-		velocity.x -= 1
-	if Input.is_action_pressed('ui_down'):
-		velocity.y += 1
-	if Input.is_action_pressed('ui_up'):
-		velocity.y -= 1
-	
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		$AnimatedSprite.play()
-	else:
-		$AnimatedSprite.stop()
-
-func _physics_process(delta):
-	get_input()
-	velocity = move_and_slide(velocity)
-
+	aniPlayer = $AnimationPlayer
+	aniTree.active = true
 
 func _process(delta):
-	pass
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength('ui_right') - Input.get_action_strength('ui_left')
+	
+
+func _physics_process(delta):
+	velocity = move_and_slide(velocity)
 
