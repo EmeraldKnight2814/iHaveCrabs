a11c429b97dadea2c7fac1d8414f71d5d20fd276
Daniel J Crowley <dcrow2814@gmail.com>
2022-03-23 23:36:49 -0500

    added crab wandering

--- Enemies/Crab.gd
+++ Enemies/Crab.gd
@@ -11,7 +11,8 @@ export var HIT_POINTS = 2
 export var FRICTION = 200
 export var VELOCITY = Vector2.ZERO
 export var ACCELERATION = 200
-export var MAX_SPEED = 80
+export var MAX_SPEED = 30
+export var WANDER_TARGET_RANGE = 4
 
 var knockback = Vector2.ZERO
 
@@ -23,6 +24,7 @@ onready var hitboxB = $HitBoxB
 onready var hurtbox = $HurtBox
 onready var scream = $WilhelmScream
 onready var zoneOfTruth = $PlayerDetectionZone
+onready var wanderController = $Wander_Controller
 
 
 func _ready():
@@ -37,18 +39,39 @@ func _physics_process(delta):
 		IDLE:
 			VELOCITY = VELOCITY.move_toward(Vector2.ZERO, FRICTION * delta)
 			seek_player()
+			if wanderController.get_time_left() == 0:
+				update_wander()
 		WANDER:
-			pass
+			seek_player()
+			if wanderController.get_time_left() == 0:
+				update_wander()
+			accelerate_towards_point(wanderController.target_position, delta)
+			if global_position.distance_to(wanderController.target_position) <= WANDER_TARGET_RANGE:
+				update_wander()
 		CHASE:
 			var player = zoneOfTruth.player
 			if player != null:
 				var direction = (player.global_position - global_position).normalized()
 				VELOCITY = VELOCITY.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+			else:
+				state = IDLE
 	VELOCITY = move_and_slide(VELOCITY)
 
 func seek_player():
 	if zoneOfTruth.can_see_player():
 		state = CHASE
+
+func accelerate_towards_point(point, delta):
+	var direction = global_position.direction_to(point)
+	VELOCITY = VELOCITY.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+
+func update_wander():
+	state = pick_random_state([IDLE, WANDER])
+	wanderController.start_wander_timer(rand_range(1, 3))
+
+func pick_random_state(state_list):
+	state_list.shuffle()
+	return state_list.pop_front()
 
 func _on_HurtBox_area_entered(area):
 	if HIT_POINTS <= 0:
