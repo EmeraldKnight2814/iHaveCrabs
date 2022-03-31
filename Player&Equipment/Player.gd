ff96d63f2025366307e638950a1e187ebda24b86
Daniel J Crowley <dcrow2814@gmail.com>
2022-03-31 00:49:17 -0500

    Update Player.gd

--- Player&Equipment/Player.gd
+++ Player&Equipment/Player.gd
@@ -60,12 +60,9 @@ func move_state(delta):
 	if Input.is_action_just_pressed("attack"):
 		state = ATTACK
 	
-func updateArmorSet(index_in, damage_in, hit_points_in):
+func updateArmorSet(index_in):
 	currentSprite = sprites[index_in]
 	armor.texture = currentSprite
-	stats.damage = damage_in
-	stats.hit_points = hit_points_in
-	stats.max_hit_points = hit_points_in
 	
 func attack_state(delta):
 	aniState.travel('attack')
